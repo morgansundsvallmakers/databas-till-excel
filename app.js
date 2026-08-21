8fdb3db235b198334c2f8f1e4755a00214c60c0d
morgansundsvallmakers <morgan@sundsvallmakers.se>
2026-08-21 15:04:19 +0200

    Bust worker cache on startup

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ function resetSelection() {
 }
 
 function initWorker() {
-  worker = new Worker("./worker.js");
+  worker = new Worker(`./worker.js?v=${Date.now()}`);
 
   worker.onmessage = (event) => {
     const data = event.data;
